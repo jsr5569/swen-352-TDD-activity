52b14374f4cd77a01e5b875f34db8cdc237f4ac2
mpb8984 <71938683+mpb8984@users.noreply.github.com>
2025-04-11 15:28:01 -0400

    SimpleBankAccount : 2 : get account balance: create passing code

--- src/main/java/edu/rit/swen352/tdd/SimpleBankAccount.java
+++ src/main/java/edu/rit/swen352/tdd/SimpleBankAccount.java
@@ -2,17 +2,16 @@ package edu.rit.swen352.tdd;
 
 public class SimpleBankAccount {
 
-    private static double balance;
+    private double balance;
 
     public SimpleBankAccount() {
-        balance = 0;
+        this.balance = 0;
     }
 
     public SimpleBankAccount(double initialBalance) {
-       balance = initialBalance;
+       this.balance = initialBalance;
     }
     public double getBalance() {
-        assert false: "NYI";
-        return -1.0;
+        return this.balance;
     }
 }
