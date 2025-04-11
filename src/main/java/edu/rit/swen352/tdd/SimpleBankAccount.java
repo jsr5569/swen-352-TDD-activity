4ce9ac4828cece4beaf3c1bd1fea6ca75a3ab1d9
mpb8984 <71938683+mpb8984@users.noreply.github.com>
2025-04-11 16:31:21 -0400

    docstrings

--- src/main/java/edu/rit/swen352/tdd/SimpleBankAccount.java
+++ src/main/java/edu/rit/swen352/tdd/SimpleBankAccount.java
@@ -1,23 +1,51 @@
 package edu.rit.swen352.tdd;
 
-import static java.lang.Math.round;
-
+/**
+ * SimpleBankAccount represents a basic checking account
+ *
+ * <p>
+ * SimpleBankAccount has these features:
+ * <ul>
+ *   <li>constructor: with or without initial balance</li>
+ *   <li>deposit: Put an amount into the bank account,
+ *   if an invalid amount then a {@link IllegalArgumentException} is thrown</li>
+ *   <li>withdraw: Remove an amount from the bank account,
+ *   if an invalid amount then a {@link IllegalArgumentException} is thrown</li>
+ *   <li>getBalance: returns the current bank account balance
+ * </ul>
+ */
 public class SimpleBankAccount {
 
     private double balance;
 
+    /**
+     * Create an empty simple bank account
+     */
     public SimpleBankAccount() {
         this.balance = 0;
     }
 
+    /**
+     * Creates a simple bank account with an initial balance
+     * @param initialBalance the initial balance
+     */
     public SimpleBankAccount(double initialBalance) {
        this.balance = initialBalance;
     }
 
+    /**
+     * Returns the current balance of the bank account
+     * @return the current balance
+     */
     public double getBalance() {
         return this.balance;
     }
 
+    /**
+     * Deposits an amount into the bank account
+     * Throws and IllegalArgumentException if an invalid amount
+     * @param amount the amount to deposit
+     */
     public void deposit(double amount){
         if(amount < 0 || Double.isNaN(amount))
         {
@@ -30,6 +58,11 @@ public class SimpleBankAccount {
         balance = roundToTwoDecimals(balance + amount);
     }
 
+    /**
+     * Withdraws an amount from the bank account
+     * Throws and IllegalArgumentException if an invalid amount
+     * @param amount the amount to withdraw
+     */
     public double withdraw(double amount){
         if(amount < 0 || Double.isNaN(amount) || (balance - amount) < 0)
         {
@@ -39,6 +72,11 @@ public class SimpleBankAccount {
         return this.balance;
     }
 
+    /**
+     * Private utility method for ensuring amount stays at 2 decimal places
+     * @param value the amount to round
+     * @return the amount rounded to two decimal places
+     */
     private static double roundToTwoDecimals(double value) {
         return Math.round(value * 100.0) / 100.0;
     }
