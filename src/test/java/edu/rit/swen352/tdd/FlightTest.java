4701e4d89966680dbe022f2d5f50ef8f8be938ad
Jrod1302 <jarredreep@gmail.com>
2025-04-13 16:19:34 -0400

    Flight : 1b : create a flight with null params : Create a failing test

--- src/test/java/edu/rit/swen352/tdd/FlightTest.java
+++ src/test/java/edu/rit/swen352/tdd/FlightTest.java
@@ -9,10 +9,18 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FlightTest {
 
     @Test
-    @DisplayName("Create an flight")
+    @DisplayName("Create a flight")
     public void testCreateFlight(){
         Flight f = new Flight("BA134", "Rochester", "Orlando", 5);
         assertNotNull(f);
         assertEquals(Flight.class.getSimpleName(), f.getClass().getSimpleName());
+    }
+
+    @Test
+    @DisplayName("Check that a NullPointerException is thrown when null parameters are supplied")
+    public void testCreateFlightNullParams(){
+        assertThrows(NullPointerException.class, () -> {
+            Flight f = new Flight(null, null, null, 0);
+        });
     }
 }
