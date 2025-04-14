e148600ac050d7ab5e772c724fa629eb1b06ce21
Domenic Mangano <domenicmangano@Domenics-MacBook-Pro.local>
2025-04-13 23:52:18 -0400

    Shoe Account : 1 : create shoeaccount constructor : create failing test

--- src/main/java/edu/rit/swen352/tdd/ShoeAccount.java
+++ src/main/java/edu/rit/swen352/tdd/ShoeAccount.java
@@ -1,4 +1,11 @@
 package edu.rit.swen352.tdd;
 
+import java.util.List;
+
 public class ShoeAccount {
+
+    public ShoeAccount() {
+        assert false;
+    }
+
 }
--- src/test/java/edu/rit/swen352/tdd/ShoeAccountTest.java
+++ src/test/java/edu/rit/swen352/tdd/ShoeAccountTest.java
@@ -1,4 +1,14 @@
 package edu.rit.swen352.tdd;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class ShoeAccountTest {
+
+    @Test
+    public void TestShoeConstructor() {
+        ShoeAccount account = new ShoeAccount();
+        assertNotNull(account);
+    }
 }
