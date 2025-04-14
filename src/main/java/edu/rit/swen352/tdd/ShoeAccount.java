e63f9468451b327fca7c7771d524d1aa32e6203c
Domenic Mangano <domenicmangano@Domenics-MacBook-Pro.local>
2025-04-13 23:53:21 -0400

    Shoe Account : 2 : create shoeaccount constructor : create passing test

--- src/main/java/edu/rit/swen352/tdd/ShoeAccount.java
+++ src/main/java/edu/rit/swen352/tdd/ShoeAccount.java
@@ -1,11 +1,19 @@
 package edu.rit.swen352.tdd;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShoeAccount {
 
+    public class Shoe {
+        double size;
+        float price;
+        String condition;
+    }
+    List<Shoe> shoes;
+
     public ShoeAccount() {
-        assert false;
+        this.shoes = new ArrayList<>();
     }
 
 }
